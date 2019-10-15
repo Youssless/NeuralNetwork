392b1914e059f771d34109f15d78f0d3f30d3c2e
Yaseen Lahmami <33607275+Youssless@users.noreply.github.com>
2019-10-15 15:27:14 +0100

    Add files via upload

---
+++ nn.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+class NeuralNetwork:
+    def __init__(self):
+        self.num_of_layers = 0
+
+    def sigmoid(self, X):
+        return 1 / (1 + np.exp(-X))
+
+    def train(self):
+        pass
+
+    def __back_propagate__():
+        pass
+
+    def __forward_propagate__():
+        pass
+
