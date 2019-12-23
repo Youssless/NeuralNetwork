c1a52b4ed755f188c7f37e2e9e9d5e8b7652772a
Youssless <yaseenlahmami98@gmail.com>
2019-12-23 22:49:08 +0000

    feed forward done

--- nn.py
+++ nn.py
@@ -1,25 +1,21 @@
 import numpy as np
 
 class NeuralNetwork:
-    def __init__(self, num_of_layers, nodes_per_layer, alpha):
-        self.num_of_layers = num_of_layers
-        self.nodes_per_layer = nodes_per_layer
-        self.alpha = alpha
+    def __init__(self, inp):
+        self.nodes_per_layer = np.shape(inp)[0]
+        self.num_of_layers = np.shape(inp)[1]
 
-        self.initial_weights = np.ones(shape=(self.num_of_layers, self.nodes_per_layer), dtype='double')
-        #print(self.initial_weights)
+        # Z = W.I , W = Weights matrix, I = Input matrix
+        # O = sigmoid(Z) , O = Sigmoid matrix
+        self.W = np.random.random((self.nodes_per_layer, self.nodes_per_layer))
+        self.I = inp
+        self.Z = None
+        self.O = None
 
-        I = np.array([0.5, 0.9, 3.5], dtype='double')
-        #print(self.sigmoid(0.99260846))
-        X = np.zeros(shape=(1, self.nodes_per_layer))
-        for i in range(self.num_of_layers):
-            X = np.dot(self.initial_weights, I.T)
-            I = X
-            O = self.sigmoid(X)
-            print(O)
+        # initial output
+        self.__forward_propagate__()
 
     def sigmoid(self, X):
-        #print(-X)
         return 1 / (1 + np.exp(-X))
 
     def train(self):
@@ -29,8 +25,24 @@ class NeuralNetwork:
         pass
 
     def __forward_propagate__(self):
-        pass
+        # Z = W.I
+        # O = sigmoid(Z)
+        self.Z = np.dot(self.W, self.I)
+        self.O = self.sigmoid(self.Z)
+
+    def log(self):
+        print('-----|Input|-----\n')
+        print(self.I, '\n')
+        print('-----|Weights|-----\n')
+        print(self.W, '\n')
+        print('-----|Pre-Sigmoid|-----\n')
+        print(self.Z, '\n')
+        print('-----|Post-Sigmoid|-----\n')
+        print(self.O, '\n')
+
 
 
 if __name__ == '__main__':
-    nn = NeuralNetwork(3, 3, 0.01)
+    inp = np.array([[0.5, 3, 0.2], [0.11, 0.54, 1.3], [0.003, 0.34, 12.54]])
+    nn = NeuralNetwork(inp)
+    nn.log()
