62d2fa5eea85a2dc7c140b7bf9630b1374023e77
Youssless <yaseenlahmami98@gmail.com>
2020-02-11 14:54:35 +0000

    network works! Needs refining, refactoring and more tests

--- nn.py
+++ nn.py
@@ -20,7 +20,7 @@ class NeuralNetwork:
                 print("model must be in the format equivalent to [2, 2, 2, etc..] which defines the number of neurons in each layer")
 
     def train(self, X, y, alpha=0.03, epochs=1000):
-        pass
+        self.back_propagate(X, y, alpha, epochs)
 
     def sigmoid(self, Z):
         return 1/(1+np.exp(-Z))
@@ -37,44 +37,32 @@ class NeuralNetwork:
             X = next_layer
         return np.array(nodes)
 
-    def back_propagate(self, X, y, alpha=0.03, epochs=1000):
-        # we need to update the weights
-        # to update the weights we need the cost and the errors of all the nodes in the network
-        # formula output and hidden layers pE/pW => -(target - output_curr_layer).sigmoid(Z_prev_layer).(1-sigmoid(Z_prev_layer)).output_prev_layer
+    def back_propagate(self, X, y, alpha, epochs):
+        for e in range(epochs):
+            nodes = self.forward_propagate(X)
+            print("epoch: {}".format(e))
+            print("cost = {}".format(self.cost(0, nodes[self.layers-1])))
+            curr_error = np.array(y - nodes[self.layers-1])
+            layer_error = None
+            errors = []
+            for i in range(self.layers-2, -1, -1):
+                errors.insert(i, curr_error)
+                layer_error = np.dot(self.weights[i].T, curr_error)
+                curr_error = layer_error
+                
+            errors.insert(0, curr_error)
+            E = np.array(errors)
 
-        # error back propagation
-        alpha = 0.03
-        nodes = self.forward_propagate(X)
-        print("cost_before = {}".format(self.cost(0, nodes[self.layers-1])))
-        curr_error = np.array(y - nodes[self.layers-1])
-        layer_error = None
-        errors = []
-        for i in range(self.layers-2, -1, -1):
-            errors.insert(i, curr_error)
-            layer_error = np.dot(self.weights[i].T, curr_error)
-            curr_error = layer_error
-            
-        errors.insert(0, curr_error)
-        E = np.array(errors)
-
-        for i in range(self.layers-1, 0, -1):
-            a = self.sigmoid(nodes[i])
-            delta_w = -(alpha*np.dot((E[i]*(a*(1-a))).reshape(-1, 1), nodes[i-1].reshape(-1, 1).T))
-            print(delta_w)
-            
-            self.weights[i-1] = self.weights[i-1] - delta_w
-
-        nodes = self.forward_propagate(X)
-        print("cost_after = {}".format(self.cost(0, nodes[self.layers-1])))
+            self.gradient_descent(nodes, alpha, E)
         
 
     def gradient_descent(self, nodes, alpha, E):
         for i in range(self.layers-1, 0, -1):
             a = self.sigmoid(nodes[i])
             delta_w = -(alpha*np.dot((E[i]*(a*(1-a))).reshape(-1, 1), nodes[i-1].reshape(-1, 1).T))
-            print(delta_w)
-            
-            self.weights[i-1] = self.weights[i-1] + delta_w
+
+            # update weights
+            self.weights[i-1] = self.weights[i-1] - delta_w
 
 
 if __name__ == "__main__":
@@ -83,15 +71,5 @@ if __name__ == "__main__":
     y = np.array([0])
     nn = NeuralNetwork(model)
 
-    # print("num layers: " + str(nn.layers))
-    # print("sigmoid test: " + str(nn.sigmoid(X)))
-    # print("cost test: " + str(nn.cost(0, nn.forward_propagate(X))))
-    # print("bias test: \n" + str(nn.bias))
-    # print("weights test: \n" + str(nn.weights))
-    # print("output test: \n" + str(nn.forward_propagate(X)))
-    # arr = nn.forward_propagate(X)
-    # [print(arr[i]) for i in range(3)]
-    # print("back propagate test: \n" + str(nn.back_propagate(X, np.array(y))))
-    # nn.gradient_descent(X, y)
-
-    nn.back_propagate(X, y)
+    nn.train(X, y, alpha=0.03, epochs=10000)
+    print(nn.forward_propagate(X))
