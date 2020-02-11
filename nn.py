02c16afd3f2e44d2651fa0c521e4c2d97656b3bf
Youssless <yaseenlahmami98@gmail.com>
2020-02-11 14:39:08 +0000

    backpropagation partially complete, cost is decreasing

--- nn.py
+++ nn.py
@@ -19,7 +19,7 @@ class NeuralNetwork:
                 print("model is not a list.")
                 print("model must be in the format equivalent to [2, 2, 2, etc..] which defines the number of neurons in each layer")
 
-    def train(self, alpha, X, y):
+    def train(self, X, y, alpha=0.03, epochs=1000):
         pass
 
     def sigmoid(self, Z):
@@ -30,19 +30,23 @@ class NeuralNetwork:
 
     def forward_propagate(self, X):
         next_layer = None
+        nodes = [X]
         for i in range(self.layers-1):
             next_layer = np.dot(self.weights[i], X) + self.bias[i]
+            nodes.insert(i+1, next_layer)
             X = next_layer
-        return next_layer
+        return np.array(nodes)
 
-    def back_propagate(self, X, y):
+    def back_propagate(self, X, y, alpha=0.03, epochs=1000):
         # we need to update the weights
         # to update the weights we need the cost and the errors of all the nodes in the network
         # formula output and hidden layers pE/pW => -(target - output_curr_layer).sigmoid(Z_prev_layer).(1-sigmoid(Z_prev_layer)).output_prev_layer
 
         # error back propagation
-        out = self.forward_propagate(X)
-        curr_error = np.array(y - out)
+        alpha = 0.03
+        nodes = self.forward_propagate(X)
+        print("cost_before = {}".format(self.cost(0, nodes[self.layers-1])))
+        curr_error = np.array(y - nodes[self.layers-1])
         layer_error = None
         errors = []
         for i in range(self.layers-2, -1, -1):
@@ -53,21 +57,24 @@ class NeuralNetwork:
         errors.insert(0, curr_error)
         E = np.array(errors)
 
-        return E.reshape(-1, 1)
-
-    def gradient_descent(self, X, y, alpha=0.03, epochs=1000):
-        for i in range(epochs):
-            E = self.back_propagate(X, y)
-            out = self.forward_propagate(X)
+        for i in range(self.layers-1, 0, -1):
+            a = self.sigmoid(nodes[i])
+            delta_w = -(alpha*np.dot((E[i]*(a*(1-a))).reshape(-1, 1), nodes[i-1].reshape(-1, 1).T))
+            print(delta_w)
             
-            for i in range(self.layers-1, -1, -1):
-                o_prev = np.dot(self.weights[i-1], out) + self.bias[i-1]
-                a = self.sigmoid(o)
-                delta_w = np.dot(-(E[i])*(a*(1-a), o_prev))
+            self.weights[i-1] = self.weights[i-1] - delta_w
 
-                out = o_prev
-                print(delta_w)
+        nodes = self.forward_propagate(X)
+        print("cost_after = {}".format(self.cost(0, nodes[self.layers-1])))
+        
 
+    def gradient_descent(self, nodes, alpha, E):
+        for i in range(self.layers-1, 0, -1):
+            a = self.sigmoid(nodes[i])
+            delta_w = -(alpha*np.dot((E[i]*(a*(1-a))).reshape(-1, 1), nodes[i-1].reshape(-1, 1).T))
+            print(delta_w)
+            
+            self.weights[i-1] = self.weights[i-1] + delta_w
 
 
 if __name__ == "__main__":
@@ -76,11 +83,15 @@ if __name__ == "__main__":
     y = np.array([0])
     nn = NeuralNetwork(model)
 
-    print("num layers: " + str(nn.layers))
-    print("sigmoid test: " + str(nn.sigmoid(X)))
-    print("cost test: " + str(nn.cost(0, nn.forward_propagate(X))))
-    print("bias test: \n" + str(nn.bias))
-    print("weights test: \n" + str(nn.weights))
-    print("output test: \n" + str(nn.forward_propagate(X)))
-    print("back propagate test: \n" + str(nn.back_propagate(X, np.array(y))))
-    nn.gradient_descent(X, y)
+    # print("num layers: " + str(nn.layers))
+    # print("sigmoid test: " + str(nn.sigmoid(X)))
+    # print("cost test: " + str(nn.cost(0, nn.forward_propagate(X))))
+    # print("bias test: \n" + str(nn.bias))
+    # print("weights test: \n" + str(nn.weights))
+    # print("output test: \n" + str(nn.forward_propagate(X)))
+    # arr = nn.forward_propagate(X)
+    # [print(arr[i]) for i in range(3)]
+    # print("back propagate test: \n" + str(nn.back_propagate(X, np.array(y))))
+    # nn.gradient_descent(X, y)
+
+    nn.back_propagate(X, y)
